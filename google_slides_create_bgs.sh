cbc5d85e84cf6c0e4d4d247d19368c4e5eb60a45
Max Melching <m-melching@web.de>
2025-03-08 17:29:42 +0100

    Recompiling slides background and main file.

--- google_slides_create_bgs.sh
+++ google_slides_create_bgs.sh
@@ -5,3 +5,4 @@
 # convert -density 300 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpi.png
 # convert -density 300 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpigrav.png
 # convert -density 300 google_slides_template.pdf google_slides_bgs/google_slides_gw_caltech.png
+# convert -density 300 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpiblue.png
