c6f460319399c030204a6123f9bde0999f6c4b05
Max Melching <m-melching@web.de>
2025-03-24 23:33:22 +0100

    Renaming create flides file for better distinguishability from other files. Also increasing resolution.

---
+++ create_google_slides_bgs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# convert -density 600 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpigrav_mixed.png
+# convert -density 600 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpi_mixed.png
+# convert -density 600 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpi.png
+# convert -density 600 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpigrav.png
+# convert -density 600 google_slides_template.pdf google_slides_bgs/google_slides_gw_caltech.png
+# convert -density 600 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpiblue.png
+# convert -density 600 google_slides_template.pdf google_slides_bgs/google_slides_gw_mpilightblue.png
+
+# convert -density 600 examples/gwtheme_frame.pdf examples/gwtheme_frame.png
